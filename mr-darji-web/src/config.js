94555a8fdf6e69776d5cd22943c856ebe5f3a559
Jatin Poriya <kiritporiya25@gmail.com>
2025-12-18 22:44:30 +0530

    build

--- mr-darji-web/src/config.js
+++ mr-darji-web/src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://172.22.208.162:5000";
-// const BASE_URL = "https://mr-darji.onrender.com";
+// const BASE_URL = "http://172.22.208.162:5000";
+const BASE_URL = "https://mr-darji.onrender.com";
 
 export default BASE_URL;
